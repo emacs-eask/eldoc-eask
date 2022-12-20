12f7f03b8ec7de2f882792ccf8c212b3bb9dcf30
Jen-Chieh Shen <jcs090218@gmail.com>
2022-12-20 18:03:33 +0800

    add src

---
+++ eldoc-eask.el
@@ -0,0 +1,41 @@
+;;; eldoc-eask.el --- Eldoc support for Eask-file  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/jcs090218/eldoc-eask
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Eldoc support for Eask-file
+;;
+
+;;; Code:
+
+(defgroup eldoc-eask nil
+  "Eldoc support for Eask-file."
+  :prefix "eldoc-eask-"
+  :group 'tool
+  :link '(url-link :tag "Repository" "https://github.com/emacs-eask/eldoc-eask"))
+
+(provide 'eldoc-eask)
+;;; eldoc-eask.el ends here
